c1dd2478e546693c3de4c9837372abe9816288ca
Diego <diegogalvaoamorim@gmail.com>
2025-09-03 19:16:09 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import modelEntities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "book");
+        System.out.println(obj);
+
+    }
+
+}
