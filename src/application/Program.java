1641b4056bdf40359a29ea85c723e8ddf3ea49e2
Diego <diegogalvaoamorim@gmail.com>
2025-09-17 17:31:07 -0300

    add class seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,23 @@
 package application;
 
 import modelEntities.Department;
+import modelEntities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         Department obj = new Department(1, "book");
-        System.out.println(obj);
+
+        Seller seller = new Seller(4454.34, LocalDate.parse("20/07/2022", fmt1), obj, "aoeofakof", 21, "af");
+
+        System.out.println(seller);
 
     }
 
